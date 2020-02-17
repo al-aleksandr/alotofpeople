4a843b0f4cc7bde0ddc567cd2d26bda0886f70e1
al-aleksandr <al.aleksandr@svitoglyad.cn.ua>
2020-02-18 00:17:26 +0200

    improve responce

--- alop_main.py
+++ alop_main.py
@@ -15,21 +15,17 @@ def get_db():
     PS_list = PalatsSportu_main.get_list()
     OS_list = OlympicStadium_google.get_list()
     db = []
-    db.append({"place" : "ДвСп", "ev_list" : PS_list})
-    db.append({"place" : "ОлСт", "ev_list" : OS_list})
+    db.append({"place" : "Дворец Спорта", "ev_list" : PS_list})
+    db.append({"place" : "Олимпийский Стадион", "ev_list" : OS_list})
     return db
 
 def get_event_by_date(dt, db):
     dt_next = dt + timedelta(days=1)
     ret = []
-    # ret.append("%s:" % (db["place"]))
     for item in db["ev_list"]:
         if (item['date'] >= dt) and (item['date'] < dt_next):
-            event = u'%s (%s):\n%s' % (item['date'].strftime("%H:%M"), db["place"], item['title'])
+            event = u'%s, %s:\n%s' % (item['date'].strftime("%H:%M"), db["place"], item['title'])
             ret.append(event)
-
-    # if len(ret) == 1:
-    #     return []
 
     return ret
 
@@ -39,6 +35,22 @@ def get_upcomming_events(count_days=7):
     day = get_date_today()
     end_day = day + timedelta(days=count_days)
     event_list = []
+    no_event = True
+
+    day_list = []
+    for db in ev_db:
+        day_ev = get_event_by_date(day, db)
+        if day_ev != []:
+            day_list.extend(day_ev)
+
+    event_list.append(day.strftime(u"----------------- %a %d/%m (Сегодня) -\n").decode('utf-8'))
+    if day_list != []:
+        event_list.extend(day_list)
+        no_event = 0
+    else:
+        event_list.append(u'Нет событий')
+
+    day += timedelta(days=1)
 
     while day < end_day:
         day_list = []
@@ -50,8 +62,12 @@ def get_upcomming_events(count_days=7):
         if day_list != []:
             event_list.append(day.strftime("----------------- %a %d/%m -----------------").decode('utf-8'))
             event_list.extend(day_list)
+            no_event = 0
 
         day += timedelta(days=1)
+
+    if no_event:
+        return [u'Нет событий на ближайшие %d дн.' %(count_days)]
 
     return event_list
 
